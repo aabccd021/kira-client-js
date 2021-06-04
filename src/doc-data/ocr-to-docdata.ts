2849775c82797b0857517d0761356fd49857a0e6
aabccd021 <aabccd021@gmail.com>
2021-06-04 16:16:44 +0700

    remove util

--- src/doc-data/ocr-to-docdata.ts
+++ src/doc-data/ocr-to-docdata.ts
@@ -1,16 +1,20 @@
 import assertNever from 'assert-never';
 
-import { DocData, OCRDocData } from '../types';
-import { mapValues } from '../util';
+import { DocData, DocField, OCRDocData, OCRDocField } from '../types';
 
 export function ocrToDocData(ocrDocData: OCRDocData): DocData {
-  return mapValues(ocrDocData, (field) => {
-    if (field.type === 'count') return 0;
-    if (field.type === 'creationTime') return new Date();
-    if (field.type === 'image') return field.value;
-    if (field.type === 'owner') return { ...field.value.user, id: field.value.id };
-    if (field.type === 'ref') return { ...field.value.doc, id: field.value.id };
-    if (field.type === 'string') return field.value;
-    assertNever(field);
-  });
+  return Object.entries(ocrDocData).reduce(
+    (acc, [fieldName, field]) => ({ ...acc, [fieldName]: ocrToDocField(field) }),
+    {}
+  );
+}
+
+function ocrToDocField(field: OCRDocField): DocField {
+  if (field.type === 'count') return 0;
+  if (field.type === 'creationTime') return new Date();
+  if (field.type === 'image') return field.value;
+  if (field.type === 'owner') return { ...field.value.user, id: field.value.id };
+  if (field.type === 'ref') return { ...field.value.doc, id: field.value.id };
+  if (field.type === 'string') return field.value;
+  assertNever(field);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './service';
 export * from './state';
 export * from './types';
-export * from './util';
 export * from 'kira-core';
