dc058820a7b021bc9df5872bc6ba970593849905
aabccd021 <aabccd021@gmail.com>
2021-08-13 17:27:29 +0700

    rm

--- lib/kv.d.ts
+++
@@ -1,8 +0,0 @@
-import { Option, StateController } from 'trimop';
-import { DB, Listen, Unsubscribe } from './type';
-export declare function reset(dbController: StateController<DB>): undefined;
-export declare function deleteRecord(dbController: StateController<DB>, key: string): undefined;
-export declare function getRecord<T>(db: StateController<DB>, key: string): Option<T>;
-export declare function setRecord<T>(dbController: StateController<DB>, key: string, newValue: Option<T>): undefined;
-export declare function subscribeToRecord<T>(dbController: StateController<DB>, key: string, newListen: Listen<T>): Unsubscribe;
-//# sourceMappingURL=kv.d.ts.map
